7e42b3a8b89c724df46c019f710cdbfff4bff2ce
Hendrik Karilaid <karilaid@greeny.cs.tlu.ee>
2015-09-18 10:59:12 +0300

    asd

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
 		
 		if ($_POST["password"] == $_POST["confirm_password"]) {
-		$password_right = "Paroolid klapivad";
+		$password_right = "Paroolid klapivadasd";
 }
 	else {
 		$password_fail = "Paroolid ei klapi.";
